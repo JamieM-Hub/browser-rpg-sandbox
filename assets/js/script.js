ef995a963c7cdaa0f57dcadbb48f873e13002cc9
JamieH-Hub <jamiemckenzie7231@yahoo.co.uk>
2021-03-21 05:29:06 +0000

    add images and style to left column

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
 $(document).ready(function () {
 
-// add style
-$(".option-button").addClass("btn btn-option")
-$(".player-options > .col-4").addClass("center-text").text("naaah")
-}
+    $("#location-background").attr("onclick", "showCoords(event)")
+
+    // add style
+    $(".option-button").addClass("btn btn-option")
+    $(".player-options > .col-4").addClass("center-text").text("naaah")
+
+    showCoords = (event) => {
+        var x = event.pageX;
+        var y = event.pageY;
+        var xx = event.offsetX;
+        var yy = event.offsetY;
+        var coords = "X coords: " + x + ", Y coords: " + y;
+        var coords2 = "X coords: " + xx + ", Y coords: " + yy;
+        console.log(coords)
+        console.log(coords2)
+        // document.getElementById("demo").innerHTML = coords;
+    }
+
+    
+})
