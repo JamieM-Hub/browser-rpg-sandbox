c62e59c8283515c2966ad6064482d9eaecd1508f
JamieH-Hub <jamiemckenzie7231@yahoo.co.uk>
2021-03-21 07:11:20 +0000

    add player object constructor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,32 @@
 $(document).ready(function () {
 
-    $("#location-background").attr("onclick", "showCoords(event)")
+    const XP_LEVELS = [0, 50, 100, 150, 200, 250];
+    const HP_START = 100
 
-    // add style
-    $(".option-button").addClass("btn btn-option")
-    //$(".player-options > .col-4").addClass("center-text").text("naaah")
-
-    showCoords = (event) => {
-        var x = event.pageX;
-        var y = event.pageY;
-        var xx = event.offsetX;
-        var yy = event.offsetY;
-        var coords = "X coords: " + x + ", Y coords: " + y;
-        var coords2 = "X coords: " + xx + ", Y coords: " + yy;
-        console.log(coords)
-        console.log(coords2)
-        // document.getElementById("demo").innerHTML = coords;
+    function Player(name, race, skill, blessing, attack, defense, speed, img) {
+        this.name = name;
+        this.race = race;
+        this.skill = skill;
+        this.blessing = blessing;
+        this.attack = attack;
+        this.defense = defense;
+        this.speed = speed;
+        this.img = img;
+        this.gold = 0;
+        this.level = 1;
+        this.XP = {
+            current: "0",
+            toNext: XP_LEVELS[1]
+        }
+        this.HP = {
+            max: HP_START,
+            current: HP_START
+        }
+        this.status = [false]
+        this.perks = []
     }
 
+    var player = new Player("Siph", "Human", "Warrior", "Fish God Love", 666, 420, 69, "assets/img/player-image.jpg");
 
+    console.log(player)
 })
